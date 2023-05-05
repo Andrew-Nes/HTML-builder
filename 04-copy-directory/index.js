b9e25b1329c9512d1141785b369c62c1963963ab
Andrei Niasmachny <Cejosoll@gmail.com>
2023-05-05 17:26:00 +0300

    feat: make task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyFiles() {
+  fs.readdir(path.dirname(__filename) + '/files/', (err, files) => {
+    if (err) throw err;
+    for  (const file of files) {
+      fs.readFile(path.dirname(__filename) + '/files/' + `${file}`, (err, data) => { 
+        if (err) throw err;
+        fs.writeFile(path.dirname(__filename) + '/files-copy/' + `${file}`, data, (err) => {
+          if(err) throw err;
+        });
+      });
+
+    }
+  });
+}
+
+function copyDir (){
+  fs.mkdir(path.dirname(__filename) + '/files-copy', (err) => {
+    if(err) {
+      fs.readdir(path.dirname(__filename) + '/files-copy', (err, files) => {if (err) throw err;
+        for  (const file of files){
+          fs.unlink(path.dirname(__filename) + '/files-copy'+ `/${file}`, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    }
+  });
+  copyFiles();
+}
+copyDir();
